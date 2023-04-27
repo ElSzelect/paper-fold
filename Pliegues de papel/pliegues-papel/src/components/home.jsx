9673b536f48423dde88373b2dbc69ffc1e824c23
ElSzelect <107444120+ElSzelect@users.noreply.github.com>
2023-04-27 18:20:50 -0300

    Langing finish, Home in progress, astronout moving

--- Pliegues de papel/pliegues-papel/src/components/home.jsx
+++ Pliegues de papel/pliegues-papel/src/components/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import '../styles/home.css'
+import imagenFondo from '../images/PapelFoldFinal.png'
+import astronout from '../images/astronout.png'
 export default function Home(props) {
   return (
-    <div>
-      <h1>Paper Folds</h1>
-      <h2>Tukis</h2>
+    <div className="landing-page text-white bg-cover bg-center h-screen" style={{ backgroundImage: `url(${imagenFondo})` }}>
+    <div className="astronauta-container relative">
+      <img src={astronout} alt="astronauta" className="astronauta " />
     </div>
+    
+  </div>
   )
 }
--- Pliegues de papel/pliegues-papel/src/components/landingPage.jsx
+++ Pliegues de papel/pliegues-papel/src/components/landingPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import '../styles/landingPage.css'
 import imagenFondo from '../images/PapelFoldFinal.png'
+import astronout from '../images/astronout.png'
 import { AiFillInstagram, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 export default function LandingPage(props) {
   return (
@@ -10,24 +12,27 @@ export default function LandingPage(props) {
         <header></header>
         <main className="py-10 text-center justify-center">
           <div className="title-container">
-            <h1 className="text-6xl font-bold">Paper Folder</h1>
+            <h1 className="bg-black bg-opacity-25  text-6xl font-bold">Paper Folder</h1>
             <h2 className="text-3xl pt-5">Proyect A4 Size</h2>
-            <h3 className="font-thin">by Ivan Szelect</h3>
+            <h3 className="font-thin code">by Ivan Szelect</h3>
           </div>
           <div className="pt-5 mx-auto text-center text-white lg:max-w-xl md:max-w-lg sm:max-w-sm">
             <h4 className="text-lg bg-gray-900/50 border-black-500 border-2 p-4">¿Sabías que si doblas 7 veces una hoja A4 tendrías el espesor de 1.28 cm de un cuaderno?<br/> No es posible doblar más que eso un papel, pero... <br/>¿y si fuera posible? <br/>Esta función devuelve las medidas y equivalencias de la cantidad de pliegues que reciba su hoja A4 de 0.1 milímetros.</h4>
+          </div>
+          <div className='py-20 '>
+            <Link to='/home' className='bg-black  transition duration-300 text-slate-200 text-3xl rounded-full hover:bg-black hover:bg-opacity-85 bg-opacity-50 px-4 py-2 hover:text-white'>Let's Start</Link>
           </div>
         </main>
           <footer className=''>
           <div className=" text-6xl flex justify-center gap-16 pu-3 text-gray-200">
             <a href="https://www.linkedin.com/in/szelect/" rel="noreferrer" target="_blank">
-              <AiFillLinkedin className="hover:text-gray-700 dark:hover:text-gray-200" />
+              <AiFillLinkedin className="hover:text-gray-500 transition duration-300" />
             </a>
             <a href="https://www.instagram.com/ivan_szelect" rel="noreferrer" target="_blank">
-              <AiFillInstagram className="hover:text-gray-700  dark:hover:text-gray-200" />
+              <AiFillInstagram className="hover:text-gray-500 transition duration-300 " />
             </a>
             <a href="https://github.com/ElSzelect" rel="noreferrer" target="_blank">
-              <AiFillGithub className="hover:text-gray-700  dark:hover:text-gray-200" />
+              <AiFillGithub className="hover:text-gray-500 transition duration-300 " />
             </a>
           </div>
 
