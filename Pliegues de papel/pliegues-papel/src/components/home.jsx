54ca504758a7196eb272bdcadc87f4ec861cd827
ElSzelect <107444120+ElSzelect@users.noreply.github.com>
2023-07-24 22:35:00 -0300

    feat clipboard

--- Pliegues de papel/pliegues-papel/src/components/home.jsx
+++ Pliegues de papel/pliegues-papel/src/components/home.jsx
@@ -9,11 +9,16 @@ export default function Home(props) {
   
   
   const [foldsData, setFoldsData] = useState({
-    folds: "",
+    folds: null,
 })
 
   const submitHandler = async (e) => {
     e.preventDefault()
+    const message = document.querySelector('#message')
+    navigator.clipboard.writeText(JSON.stringify(foldsData.folds))
+    message.innerHTML = 'DONE!'
+    setTimeout(() => message.innerHTML = '', 1000)
+    
   }
 
   const inputHandler = async (e) => {
@@ -24,6 +29,8 @@ export default function Home(props) {
     
   }
 
+  
+
 
   return (
     <main className="landing-page text-white bg-cover bg-center h-screen" style={{ backgroundImage: `url(${imagenFondo})` }}>
@@ -32,11 +39,12 @@ export default function Home(props) {
           <img src={astronout} alt="astronauta" className="astronauta h-auto max-w-sm filter:shadow-lg dark:shadow-black/30 " />
         </div>
         <form onSubmit={submitHandler} className='z-10 justify-center border-white/80 bg-slate-50/10 self-center h-1/2 w-1/2 flex flex-col items-center shadow-lg shadow-black/50' action="">
-          <label>Cantidad de Pliegues</label>
-          <input onChange={inputHandler} className=' text-black border-black border-2 font-medium h-10 w-[80%] my-4 shadow-lg shadow-black/50 text-center ' type='number' placeholder=' 1,2,3,4..'></input>
-          <button className='border-black/25 border-2 p-2 text-black shadow-lg shadow-black/50 hover:bg-white transition' type='submit'>Submit</button>
+          <label>Number of Folds</label>
+          <input onChange={inputHandler} className='folds selection:bg-transparent focus:outline-none text-black border-black border-2 font-medium h-10 w-[80%] my-4 shadow-lg shadow-black/50 text-center ' type='number' placeholder=' 1,2,3,4..'></input>
+          <button className='border-black/25 border-2 p-2 text-black shadow-lg shadow-black/50 hover:bg-white active:bg-black/50 transition' type='submit'>Copy Resutls</button>
+          <span id="message" className='text-black font-bold'></span>
+          {foldsData.folds === null ?  null : <div className='z-10 self-center max-w-[50vw] bg-black/50 p-2 mt-2 content'><p>{`Number of Papers: ${foldsData.folds.CantidadHojas} Distance: ${foldsData.folds.Espesor} Unit: ${foldsData.folds.UnidadMedida}`}</p></div>}
         </form>
-          {foldsData.folds === "" ?  null : <div className='z-10 self-center max-w-[50vw] bg-black/50 p-2 mt-2 content'><p>{`Number of Papers: ${foldsData.folds.CantidadHojas} Distance: ${foldsData.folds.Espesor} Unit: ${foldsData.folds.UnidadMedida}`}</p></div>}
       </div>
     </main>
   )
